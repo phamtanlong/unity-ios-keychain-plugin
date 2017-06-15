23cdf915fb5ca43c60197672eb22a12d7440997f
Ted <ted@drpanda.com>
2017-06-15 16:43:59 +0800

    Added extra checks to remove error messages in the editor

--- Assets/Plugins/KeyChain.cs
+++ Assets/Plugins/KeyChain.cs
@@ -3,62 +3,59 @@ using System.Runtime.InteropServices;
 
 public class KeyChain 
 {	
-	#if UNITY_IPHONE || UNITY_STANDALONE_OSX
-
+	#if UNITY_IPHONE
 	[DllImport("__Internal")] private static extern void initialize(string serviceID);
 	[DllImport("__Internal")] private static extern string getString(string key, string defaultValue);
 	[DllImport("__Internal")] private static extern void setString(string key, string value);	
 	[DllImport("__Internal")] private static extern void deleteKey(string key);	
 	[DllImport("__Internal")] private static extern void printOutAllItems();
-
 	#endif
 
 	public static void Initialize()
 	{
-		#if UNITY_IPHONE || UNITY_STANDALONE_OSX
-
-		string bundleID = Application.identifier;
-		initialize(bundleID); 
-		printOutAllItems();
-
-		#else
-		throw new System.NotImplementedException();
+		#if UNITY_IPHONE
+		if(Application.platform == RuntimePlatform.IPhonePlayer)
+		{
+			string bundleID = Application.identifier;
+			initialize(bundleID); 
+			printOutAllItems();
+		}
 		#endif
 	}
 
 	public static string GetString(string key, string defaultValue = "")
 	{
-		#if UNITY_IPHONE || UNITY_STANDALONE_OSX 
-		return getString(key, defaultValue); 
+		#if UNITY_IPHONE
+		if(Application.platform == RuntimePlatform.IPhonePlayer)
+		{ return getString(key, defaultValue); }
+		else
+		{ return ""; }
 		#else
-		throw new System.NotImplementedException();
+		return "";
 		#endif
 	}
 
 	public static void SetString(string key, string value)
 	{ 
-		#if UNITY_IPHONE || UNITY_STANDALONE_OSX 
-		setString(key, value); 
-		#else
-		throw new System.NotImplementedException();
+		#if UNITY_IPHONE
+		if(Application.platform == RuntimePlatform.IPhonePlayer)
+		{ setString(key, value); }
 		#endif
 	}
 
 	public static void DeleteKey(string key)
 	{
-		#if UNITY_IPHONE || UNITY_STANDALONE_OSX  
-		deleteKey(key); 
-		#else
-		throw new System.NotImplementedException();
+		#if UNITY_IPHONE
+		if(Application.platform == RuntimePlatform.IPhonePlayer)
+		{ deleteKey(key); }
 		#endif
 	}
 
 	public static void PrintOutAllKeys()
 	{
-		#if UNITY_IPHONE || UNITY_STANDALONE_OSX 
-		printOutAllItems();
-		#else
-		throw new System.NotImplementedException();
+		#if UNITY_IPHONE
+		if(Application.platform == RuntimePlatform.IPhonePlayer)
+		{ printOutAllItems(); }
 		#endif
 	}
 }
